c29fe5007948b2c20322e1434b4c9ccf07d0147c
rockycsumb <rocky@solidrockdesigns.io>
2023-11-06 13:58:59 -0800

    init files

---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Logo from './Logo'
+import DarkModeToggle from './ui/DarkModeToggle'
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+        <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl
+        mx-auto">
+            <Logo />
+
+            <div className="flex-1 flex items-center justify-end space-x-4">
+              {/*lang selct */}
+
+              {/* session && ... */}
+
+              <DarkModeToggle />
+              {/* userbutton */}
+            </div>
+        </nav>
+        {/* upgrade */}
+    </header>
+  )
+}
+
+export default Header
---
+++ components/Logo.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import LogoImage from "@logos/talk.svg"
+import Link from 'next/link'
+import { AspectRatio } from './ui/aspect-ratio'
+import Image from "next/image"
+
+function Logo() {
+  return (
+    <Link href="/" prefetch={false} className="overflow-hidden">
+      <div className='flex items-center w-72 h-14'>
+        <AspectRatio
+        ratio={16 / 9}
+        className="flex items-center justify-center"
+        >
+        <Image
+          priority
+          src={LogoImage}
+          alt="all chat"
+          className="dark:filter dark:invert"
+        />
+
+        </AspectRatio>
+      </div>
+    </Link>
+  )
+}
+
+export default Logo
